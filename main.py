49204beb97af7cad6fdc0e0d3dc864c1af8d1ee5
Salsabil Bahaa Eldin <152950734+salsabilbahaa@users.noreply.github.com>
2025-10-09 18:04:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import LZ77 as cp
 while True:
     print('Welcome to LZ77\n')
 
-    choice=input("What would you like to do ?\n1.Compress\n2.decompress\n3.Exit\n")
+    choice=input("What would you like to do ?\n1.Compress\n2.Decompress\n3.Exit\n")
 
     compressor = cp.LZ77()
 
