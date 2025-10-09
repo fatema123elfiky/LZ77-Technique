1e56c31a00c4c4152eaf5285aed97d6f60d7dd94
salsabil <salsabilbahaa2005@gmail.com>
2025-10-09 18:48:27 +0300

    formatting

--- LZ77.py
+++ LZ77.py
@@ -1,108 +1,97 @@
 from numpy.random.mtrand import Sequence
 
-
 class LZ77:
     """That class is responsible for compressing and decompressing using data LZ77."""
+
     def __init__(self):
         """Just default constructor"""
-        self.size= 11
+        self.size = 11
         self.SearchWindow = ""
         self.LookAheadWindow = []
 
-    def Stringify(self,Tags):
+    def stringify(self, Tags):
         """Stringify is taking list of tuples and print them in the way of tags and return a string of tags
         @:parameter list of tuples of tags
         @:return string of tags
         """
-        StringifiedTags = ""
+        stringifiedTags = ""
         for Tag in Tags:
-            StringifiedTags+=(f"<{Tag[0]},{Tag[1]},{Tag[2]}> ")
-        return StringifiedTags
+            stringifiedTags += f"<{Tag[0]},{Tag[1]},{Tag[2]}> "
+        return stringifiedTags
 
-    def Compressor(self):
+    def compressor(self):
         """Compress the input using LZ77 algorithm.
         @:parameter empty
         @:return list of tuples of tags
         """
-        data=input("Enter data to be compressed: ")
+        data = input("Enter data to be compressed: ")
         CompressedData = []
 
-
-        if(self.size> len(data)):
+        if len(data) < self.size:
             self.size = len(data)
 
         LeftA, RightA = 0, self.size
         LeftS, RightS = 0, 0
-        self.LookAheadWindow=data[LeftA:RightA]
-        self.SearchWindow=data[LeftS:RightS]
+        self.LookAheadWindow = data[LeftA:RightA]
+        self.SearchWindow = data[LeftS:RightS]
 
         Sequence = ""
-        Sequence+=(self.LookAheadWindow[0])
+        Sequence += (self.LookAheadWindow[0])
 
-
-        while (LeftA<len(data)) and len(self.LookAheadWindow) :
+        while (LeftA < len(data)) and len(self.LookAheadWindow):
 
             startOfSequence = LeftA
-            Found=False
+            found = False
 
-            while (Sequence in self.SearchWindow) and (LeftA < RightA) :
-                Found=True
-                LeftA+=1
-                if(RightA< len(data)):
-                    RightA+=1
-                if(LeftA<RightA):
-                    Sequence+=(self.LookAheadWindow[LeftA-startOfSequence])
+            while (Sequence in self.SearchWindow) and (LeftA < RightA):
+                found = True
+                LeftA += 1
+                if RightA < len(data):
+                    RightA += 1
+                if LeftA < RightA:
+                    Sequence += (self.LookAheadWindow[LeftA - startOfSequence])
 
-
-            #LookAhead updates
-            LeftA+=1
-            if (RightA < len(data)):
+            # LookAhead updates
+            LeftA += 1
+            if RightA < len(data):
                 RightA += 1
-            if(LeftA<RightA):
-                self.LookAheadWindow=data[LeftA:RightA]
+            if LeftA < RightA:
+                self.LookAheadWindow = data[LeftA:RightA]
             else:
-                self.LookAheadWindow=[]
+                self.LookAheadWindow = []
 
-            # forloop to get the position and add to tags
-            NextSymbol =Sequence[-1]
-            Sequence=Sequence[:-1]
+            # For loop to get the position and add to tags
+            NextSymbol = Sequence[-1]
+            Sequence = Sequence[:-1]
 
-            if Found :
-                Pos=-1
-                for idx in range(LeftS,RightS):
-                    if(Sequence == self.SearchWindow[ idx:( idx+len(Sequence) ) ]):
-                        Pos=max(idx,Pos)
+            if found:
+                Pos = -1
+                for idx in range(LeftS, RightS):
+                    if Sequence == self.SearchWindow[idx:(idx + len(Sequence))]:
+                        Pos = max(idx, Pos)
 
-                tag = tuple((startOfSequence-Pos,len(Sequence),NextSymbol))
-            else :
+                tag = tuple((startOfSequence - Pos, len(Sequence), NextSymbol))
+            else:
                 tag = tuple((0, 0, NextSymbol))
             CompressedData.append(tag)
 
-            #update the new sequence
-            Sequence=""
-            if(len(self.LookAheadWindow)):
+            # Update the new sequence
+            Sequence = ""
+            if len(self.LookAheadWindow):
                 Sequence += self.LookAheadWindow[0]
-            else :
+            else:
                 break
 
-            # part of changing search buffer
-            if (LeftA - LeftS <= len(data)):
+            # Part of changing search buffer
+            if LeftA - LeftS <= len(data):
                 RightS = LeftA
             else:
                 RightS = LeftA
-                LeftS = RightS-self.size
+                LeftS = RightS - self.size
 
             self.SearchWindow = data[LeftS:RightS]
 
-
-
-
         return CompressedData
-
-
-
-
-
 
     def decompress(self):
         """Decompress the input using LZ77 algorithm.
@@ -110,7 +99,7 @@ class LZ77:
         @:return original text
         """
         user_input = input("Enter compressed triples like <0,0,A> <0,0,B> <2,9,>\n=> ")
-    
+
         compressed_data = []
         for part in user_input.split():
             part = part.strip("<>")
@@ -118,7 +107,7 @@ class LZ77:
             pos = int(pos)
             length = int(length)
             compressed_data.append((pos, length, char))
-    
+
         output = ""
         for position, length, next_char in compressed_data:
             if position == 0 and length == 0:
@@ -131,5 +120,3 @@ class LZ77:
 
         print("\nDecompressed text:", output)
         return output
-        
-
--- main.py
+++ main.py
@@ -3,18 +3,17 @@ import LZ77 as cp
 while True:
     print('Welcome to LZ77\n')
 
-    choice=input("What would you like to do ?\n1.Compress\n2.Decompress\n3.Exit\n")
+    choice = input("What would you like to do ?\n1.Compress\n2.Decompress\n3.Exit\n")
 
     compressor = cp.LZ77()
 
-    if choice=='1':
-        Tags=compressor.Compressor()
-        print(compressor.Stringify(Tags))
-    elif choice=='2' :
+    if choice == '1':
+        Tags = compressor.compressor()
+        print(compressor.stringify(Tags))
+    elif choice == '2':
         compressor.decompress()
-    elif choice=='3' :
+    elif choice == '3':
         print('Thanks for using LZ77')
         break
     else:
         print('Invalid choice')
-
